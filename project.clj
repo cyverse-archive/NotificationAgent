396311003d17136b0372145a1b606d783e025885
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-06-27 10:22:26 -0700

    CORE-4288: Added iplant-clavin as a dependency.

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
   :extra-classpath-dirs ["conf/test"]
   :iplant-rpm {:summary "iPlant Notification Agent"
                :provides "notificationagent"
-               :dependencies ["iplant-service-config >= 0.1.0-5"]
+               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin"]
                :config-files ["log4j.properties"]
                :config-path "conf/main"}
   :aot [notification-agent.core]
